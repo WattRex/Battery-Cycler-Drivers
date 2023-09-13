adadd4e05e855ee6fbc7ec1eccbf79ca3994c05e
marius <marius.crisan@csic.es>
2023-09-13 11:46:23 +0100

    Writter drv_flow

---
+++ code/drv_flow/src/wattrex_driver_flow/drv_flow_writter.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+'''
+Example of use of drv flow.
+'''
+#######################        MANDATORY IMPORTS         #######################
+from __future__ import annotations
+
+#######################         GENERIC IMPORTS          #######################
+from time import sleep, time
+import csv
+from datetime import datetime
+from signal import signal, SIGINT
+
+#######################       THIRD PARTY IMPORTS        #######################
+
+#######################      SYSTEM ABSTRACTION IMPORTS  #######################
+#from system_logger_tool import SysLogLoggerC, sys_log_logger_get_module_logger
+#if __name__ == '__main__':
+#    cycler_logger = SysLogLoggerC()
+#log = sys_log_logger_get_module_logger(__name__)
+
+
+#######################          PROJECT IMPORTS         #######################
+
+#######################          MODULE IMPORTS          #######################
+from .drv_flow import DrvFlowDeviceC
+#######################              ENUMS               #######################
+
+#######################             CLASSES              #######################
+
+class DrvFlowWriter():
+    def __init__(self, serial_port : str = '/dev/ttyACM0', data_file : str = 'data.csv') -> None:
+        self.data = {
+            "timestamp": [],
+            "flow_p": [],
+            "flow_n": []            
+        }   
+        self.drv = DrvFlowDeviceC (serial_port= serial_port)
+        self.file = open(data_file, 'w')
+        self.csv_writer = csv.writer(self.file)
+        self.end_condition = False
+        
+        # Write headers
+        self.csv_writer.writerow(list(self.data.keys()))
+        #csv_writer.writerow(list(self.data.keys()))
+
+        signal(SIGINT, self.signal_handler)
+
+    def signal_handler(self, sig, frame):
+        print("Finishing...")
+        self.end_condition = True
+
+    def run(self) -> None:
+        # Start recording meas and save it periodically on csv
+        time_ini = time()
+        time_save = 10
+        while not self.end_condition:
+            print('Requesting measures...')
+            measure = self.drv.get_meas()
+
+            if measure is not None:
+                dt = datetime.now()
+                self.data["timestamp"].append(dt.strftime("%m/%d/%y %H:%M:%S"))
+                self.data["flow_p"].append(measure.flow_p)
+                self.data["flow_n"].append(measure.flow_n)
+
+            if time() - time_ini > time_save:
+                self.write_csv()
+                time_ini = time()
+            sleep(1)
+        
+        self.close()
+    
+    def write_csv(self) -> None:
+        print("Saving data on csv...")
+        csv_data= list(map(lambda x, y, z: [x, y, z], self.data["timestamp"], self.data["flow_p"], self.data["flow_n"]))
+        print(csv_data)
+        self.csv_writer.writerows(csv_data)
+        self.file.flush()
+        self.data['timestamp'].clear()
+        self.data['flow_n'].clear()
+        self.data['flow_p'].clear()
+
+    def close(self) -> None:
+        print("Closing system")
+        self.write_csv()
+        self.file.close()
+        self.drv.close()
--- code/drv_flow/tests/example.py
+++ code/drv_flow/tests/example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 '''
 Example of use of drv flow.
 '''
@@ -6,12 +6,10 @@ Example of use of drv flow.
 from __future__ import annotations
 
 #######################         GENERIC IMPORTS          #######################
-from time import sleep
-from os import path
-from signal import signal, SIGINT
+import os
+import sys
 
 #######################       THIRD PARTY IMPORTS        #######################
-from enum import Enum
 
 #######################      SYSTEM ABSTRACTION IMPORTS  #######################
 #from system_logger_tool import SysLogLoggerC, sys_log_logger_get_module_logger
@@ -21,62 +19,15 @@ from enum import Enum
 
 
 #######################          PROJECT IMPORTS         #######################
+sys.path.append(os.getcwd()+'/code/')
+from drv_flow.src.wattrex_driver_flow import DrvFlowWriter
 
 #######################          MODULE IMPORTS          #######################
 
 #######################              ENUMS               #######################
 
 #######################             CLASSES              #######################
-# import time
-import csv
-
-from drv_flow import DrvFlowDeviceC
-
-end_condition = False
-
-def signal_handler(sig, frame):
-    print("Finishing...")
-    global end_condition
-    end_condition = True
-
-def main():
-    drv = DrvFlowDeviceC ()
-    data = {
-        "flow_p": [],
-        "flow_n": []
-    }        
-
-    signal(SIGINT, signal_handler)
-
-    # Open file for writing data
-    file = open('data.csv', 'w')
-    writer = csv.writer(file)
-
-    # Write headers
-    writer.writerow(list(data.keys()))
-
-    # Start recording meas and save it periodically on csv
-    iter = 0
-    while not end_condition:
-        iter += 1
-        #print('Requesting measures...')
-        measure = drv.get_meas()
-
-        if measure is not None:
-            data["flow_p"].append(measure.flow_p)
-            data["flow_n"].append(measure.flow_n)
-
-        sleep(0.5)
-
-
-    # Save data on csv
-    # Mix lists of dict to store on csv
-    print("Saving data on csv...")
-    csv_data= list(map(lambda x, y: [x, y], data["flow_p"], data["flow_n"]))
-    print(csv_data)
-    writer.writerows(csv_data)
-    file.close()
-    drv.close()
 
 if __name__ == '__main__':
-    main()
+    node = DrvFlowWriter(serial_port='/dev/ttyACM0', data_file='data.csv')
+    node.run()
