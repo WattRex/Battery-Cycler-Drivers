1f76af5f47ad039c79e5e2477c9d10c4b93084de
marius.crisan <marius.crisan@csic.es>
2023-10-17 14:53:37 +0200

    insert new enums to init

--- code/drv_base/src/wattrex_driver_base/__init__.py
+++ code/drv_base/src/wattrex_driver_base/__init__.py
@@ -2,8 +2,13 @@
 This file specifies what is going to be exported from this module.
 In this case is drv_base
 '''
-from .drv_base_status import DrvBaseStatusC, DrvBaseStatusE
+from .drv_base_status import DrvBasePwrModeE, DrvBasePwrDataC, DrvBasePwrPropertiesC, DrvBasePwrDeviceC, DrvBaseStatusC, DrvBaseStatusE
 
 __all__ = [
-    'DrvBaseStatusC', 'DrvBaseStatusE'
+    'DrvBasePwrModeE',
+    'DrvBasePwrDataC',
+    'DrvBasePwrPropertiesC',
+    'DrvBasePwrDeviceC',
+    'DrvBaseStatusC',
+    'DrvBaseStatusE'
 ]
