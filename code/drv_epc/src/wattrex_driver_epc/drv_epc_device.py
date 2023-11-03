20ebdcd76264833306aa46eaf5fb871a56aa9cff
RobertoAldea <r.aldea.csic@gmail.com>
2023-11-03 11:26:45 +0100

    Dev id should be public attribute not private

--- code/drv_epc/src/wattrex_driver_epc/drv_epc_device.py
+++ code/drv_epc/src/wattrex_driver_epc/drv_epc_device.py
@@ -53,9 +53,9 @@ class DrvEpcDeviceC : # pylint: disable= too-many-public-methods
     def __init__(self, can_id: int, dev_id: int|None = None) -> None:
         self.__device_handler: SysShdIpcChanC
         if dev_id is None:
-            self.__dev_id= can_id #pylint: disable=unused-private-member
+            self.dev_id= can_id #pylint: disable=unused-private-member
         else:
-            self.__dev_id= dev_id #pylint: disable=unused-private-member
+            self.dev_id= dev_id #pylint: disable=unused-private-member
         if not 'TX_CAN' in run(['ls', '/dev/mqueue/'], stdout= PIPE, check= False).stdout.decode():
             log.error("The can transmission queue doesn`t exist, please launch first the can node")
             raise RuntimeError(("The can transmission queue doesn`t exist, "
