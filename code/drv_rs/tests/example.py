0ed3a616635eaf42d9d48097dc28e277d96c81c1
RobertoAldea <r.aldea.csic@gmail.com>
2024-01-10 12:21:08 +0100

    Update example with new version

--- code/drv_rs/tests/example.py
+++ code/drv_rs/tests/example.py
@@ -4,7 +4,7 @@ Example to ea power supply.
 '''
 #######################        MANDATORY IMPORTS         #######################
 from __future__ import annotations
-from sys import path
+import sys
 import os
 
 #######################         GENERIC IMPORTS          #######################
@@ -13,25 +13,23 @@ import time
 #######################       THIRD PARTY IMPORTS        #######################
 
 #######################      SYSTEM ABSTRACTION IMPORTS  #######################
-path.append(os.getcwd())
+sys.path.append(os.getcwd())
 from system_logger_tool import SysLogLoggerC, sys_log_logger_get_module_logger # pylint: disable=wrong-import-position
 if __name__ == '__main__':
-    cycler_logger = SysLogLoggerC(file_log_levels='./log_config.yaml')
+    cycler_logger = SysLogLoggerC(file_log_levels= 'code/log_config.yaml')
 log = sys_log_logger_get_module_logger(__name__)
 
 #######################          PROJECT IMPORTS         #######################
-from scpi_sniffer import *
-from serial import PARITY_ODD, EIGHTBITS, STOPBITS_ONE
+from scpi_sniffer import DrvScpiSerialConfC
+from serial import PARITY_ODD
 
 #######################          MODULE IMPORTS          #######################
-from drv_rs.src.wattrex_driver_rs import *
+sys.path.append(os.getcwd()+'/code/drv_rs/')
+from src.wattrex_driver_rs import DrvRsDeviceC
 # from wattrex_driver_rs import DrvRsDeviceC
 
 #######################              ENUMS               #######################
-__SERIAL_PORT = '/dev/ttyACM0'
-__RX_CHAN_NAME = 'rx_scpi_load'
-__SCPI_MAX_MSG = 300          # messages per queue
-__SCPI_MAX_MESSAGE_SIZE = 400 # bytes per msg
+__SERIAL_PORT = '/dev/wattrex/loads/RS_79E047AE41D5'
 
 
 #######################             CLASSES              #######################
@@ -39,27 +37,48 @@ def main():
     '''
     Example usage of drv_rs with a load rs device.
     '''
-    source_conf_scpi = DrvScpiSerialConfC(port = __SERIAL_PORT,
-                                          separator = '\n',
-                                          baudrate = 115200,
-                                          bytesize = EIGHTBITS,
-                                          parity = PARITY_ODD,
-                                          stopbits = STOPBITS_ONE,
-                                          timeout = 0.5,
-                                          write_timeout = None,
-                                          inter_byte_timeout  = None)
+    load_conf_scpi = DrvScpiSerialConfC(port = __SERIAL_PORT,separator='\n',
+                                    baudrate=115200, timeout=1, write_timeout=1, parity= PARITY_ODD)
 
 
-    load_rs = DrvRsDeviceC(config = source_conf_scpi, rx_chan_name = __RX_CHAN_NAME)
-    log.info(f"Properties: {load_rs.properties.model}")
-    load_rs.set_cv_mode(volt_ref = 20000)
-    data = load_rs.get_data()
-    log.warning(f"Mode: {data.mode} - Voltage: {data.voltage} - Current: {data.current}")
-    time.sleep(3)
-    load_rs.set_cc_mode(curr_ref = 1000)
-    data = load_rs.get_data()
-    log.warning(f"Mode: {data.mode} - Voltage: {data.voltage} - Current: {data.current}")
-    load_rs.close()
+    load_rs = DrvRsDeviceC(config = load_conf_scpi)
+    try:
+        data = load_rs.get_data()
+        input("Press Enter to continue...")
+        log.info(f"Properties: {load_rs.properties.model}")
+        data = load_rs.get_data()
+        log.warning((f"Mode: {data.mode} - Voltage: {data.voltage} - Current: {data.current} "
+                     f"- Power: {data.power}"))
+        time.sleep(3)
+        load_rs.set_cc_mode(curr_ref = 200)
+        for _ in range(10):
+            #Obtain data
+            init = time.time()
+            data = load_rs.get_data()
+            log.info(f"Voltage: {data.voltage}\tCurrent: {data.current}\tPower: {data.power}\n\
+                    Mode: {data.mode}\tStatus: {data.status}")
+            log.info(f"Time elapsed: {time.time() - init}")
+            if time.time() - init< 1:
+                time.sleep(1-(time.time()-init))
+        load_rs.disable()
+        log.info("Disabled")
+        input("Press Enter to continue...")
+        load_rs.set_cv_mode(volt_ref = 3900)
+        for _ in range(10):
+            #Obtain data
+            init = time.time()
+            data = load_rs.get_data()
+            log.info(f"Voltage: {data.voltage}\tCurrent: {data.current}\tPower: {data.power}\n\
+                    Mode: {data.mode}\tStatus: {data.status}")
+            log.info(f"Time elapsed: {time.time() - init}")
+            if time.time() - init< 1:
+                time.sleep(1-(time.time()-init))
+        load_rs.close()
+        log.info('End of example of drv rs')
+    except (KeyboardInterrupt, AttributeError, Exception) as err:
+        load_rs.close()
+        log.info(f"End of example of drv rs with error: {err}")
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
